70e73019422ba907211fcc4330e21a9b069bdf52
Adam Błaszczyk <blaszczyk.adam@gmail.com>
2019-03-21 14:14:07 +0100

    Update 01-kivy-ins.sh

--- 01-kivy-ins.sh
+++ 01-kivy-ins.sh
@@ -6,6 +6,9 @@
 
 sudo add-apt-repository ppa:kivy-team/kivy
 sudo apt-get update
+
+sudo apt-get install python
+
 sudo apt-get install python-kivy
 sudo apt-get install kivy-examples
 python /usr/share/kivy-examples/canvas/canvas_stress.py
